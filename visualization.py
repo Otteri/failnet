651ac498db03603ed1de3abc80ed7fc6ca72f70a
Joni Airaksinen <airakjo@gmail.com>
2021-04-05 13:05:48 +0300

    Use Agg for creating plots
    Plots are not rendered, so now plots can be generated with terminal only
    without getting stuck

--- visualization.py
+++ visualization.py
@@ -2,23 +2,19 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from model import Channel
+matplotlib.use('Agg') # Works with console only setup (does not render plot)
 
-def plot(input_data, filtered_input, learned, iteration, invert=False) -> None:
+def plot(input_data, filtered_input, learned, iteration) -> None:
     plt.figure(figsize=(15,7))
     plt.xlabel("X", fontsize=18, labelpad=5)
     plt.ylabel("Y", fontsize=18, labelpad=5)
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
     
-    x = np.arange(input_data.size(2)) # input_length
+    x = np.arange(input_data.data.size(2)) # input_length
     plt.plot(x, input_data[0, Channel.SIG1, :], "-", color='b', label="input")
     plt.plot(x, filtered_input[0, Channel.SIG1, :], '--', color='r', label="filtered input (f)")
-    
-    if invert: # (mirrors the signal horizontally)
-        plt.plot(x, -learned[0, :], '-', color='green', label="learning outcome (l)")
-        plt.plot(x, input_data[0, Channel.SIG1, :] - learned[0, :], '-', color='black', label="f * l")
-    else:
-        plt.plot(x, learned[0, :], '-', color='green', label="learning outcome (l)")
+    plt.plot(x, learned[0, :], '-', color='green', label="learning outcome (l)")
     
     plt.legend()
 
